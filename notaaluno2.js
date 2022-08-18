c6bcf137223bb6c97987f73804a518a58ee45e41
DanielCorreia14 <107889248+DanielCorreia14@users.noreply.github.com>
2022-08-18 18:38:37 -0300

    adicionando cod 1 e 2

--- notaaluno2.js
+++ notaaluno2.js
@@ -1,10 +1,11 @@
 function notaAluno(){
     var nota1 = parseFloat(document.getElementById("nota 1").value);
     var nota2 = parseFloat(document.getElementById("nota 2").value);
-    var nota3 = parseFloat(document.getElementById("nota 3").value);
-    var media = ((nota1+nota2+nota3)/3);
-    
-    media > 7 ? "aprovado" : reprovado;
+
+    var notamin;
+
+    notamin = (nota1+nota2);
+    var x = 21 - notamin;
+    alert("a nota que precisará tirar para passar com 7 eh:"+x);
     
 }
-    
