e7696f51b3bd754447f342678e80688cccc8d609
DanielCorreia14 <107889248+DanielCorreia14@users.noreply.github.com>
2022-08-18 18:18:41 -0300

    adicionando cod 1

---
+++ notaaluno.js
@@ -0,0 +1,16 @@
+function notaAluno(){
+    var nota1 = parseFloat(document.getElementById("nota 1").value);
+    var nota2 = parseFloat(document.getElementById("nota 2").value);
+    var nota3 = parseFloat(document.getElementById("nota 3").value);
+    var media = ((nota1+nota2+nota3)/3);
+    
+    media > 7 ? "aprovado" : reprovado;
+    
+    
+    If(imc < 18)
+    ("Cuidado! você precisa aumentar seu peso")}
+    Else
+    alert("Parabens, você está dentro da media”);
+    Else
+    alert("Cuidado! você precisa diminuir seu peso")}
+    
