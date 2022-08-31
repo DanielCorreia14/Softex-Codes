e9f6d44491d46ebc1d62b73d7fc681157304cbad
Daniel Mangabeira Correia <107889248+DanielCorreia14@users.noreply.github.com>
2022-08-31 15:33:15 -0300

    Create main.java

---
+++ packageJava/main.java
@@ -0,0 +1,21 @@
+package com.facebook.www;
+  public class Conta{
+
+ public int id;
+ public String nome[75];
+ public string senha[15];
+
+ }
+ 
+package br.com.bancointer.www;
+  public class Funcionario{
+
+public int id_funcionario;
+public double hr_dia;
+public String nome[75];
+public double salario;
+public char senha_func;
+
+ }
+
+//Proposta apenas didática
