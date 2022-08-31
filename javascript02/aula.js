86a752ad9032d2008cba2285f607d6f454df1fa2
Daniel Mangabeira Correia <107889248+DanielCorreia14@users.noreply.github.com>
2022-08-31 15:41:22 -0300

    Create aula.js

---
+++ javascript02/aula.js
@@ -0,0 +1,29 @@
+var x = 10;
+var y = 5;
+
+
+
+var tt = prompt("qual operação deseja fazer?, 1 - soma, 2 - subtracao, 3 - multiplicacao, 4 - divisao");
+
+if (tt = 1){
+    
+    r = x+y;
+}
+
+else if(tt = 2){
+    r = x-y;
+}
+
+else if(tt = 3){
+    r = x*y;
+
+}
+
+else if (tt = 4){
+r = (x/y); 
+}
+else{
+    console.log("numero invalido");
+}
+
+console.log("%d",r);
