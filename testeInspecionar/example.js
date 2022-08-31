71811c8dd7357af44dbd49b11ef8bbf605c8c5dd
Daniel Mangabeira Correia <107889248+DanielCorreia14@users.noreply.github.com>
2022-08-31 15:38:31 -0300

    Create example.js

---
+++ testeInspecionar/example.js
@@ -0,0 +1,14 @@
+document.querySelector("#search")
+//Botão de pesquisa do youtube
+
+document.querySelector("#subscribe-button > ytd-subscribe-button-renderer > tp-yt-paper-button > yt-formatted-string")
+
+//Botão de inscrever-se do youtube
+
+document.querySelector("#yDmH0d > c-wiz > div > div > div.bg6sud > div:nth-child(3) > div > a")
+
+//Botão Agenda do google classroom
+
+document.querySelector("#yDmH0d > c-wiz > div > div > div > div > div:nth-child(2) > c-wiz:nth-child(3) > div > div:nth-child(4) > div > span > a > div.BvDXcd > div")
+
+//Botão para adicionar conta no google classroom
