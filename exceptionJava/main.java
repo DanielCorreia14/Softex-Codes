2bd213637ac0d584ad716a7b14f124615dee0637
Daniel Mangabeira Correia <107889248+DanielCorreia14@users.noreply.github.com>
2022-08-31 15:37:34 -0300

    Create main.java

---
+++ exceptionJava/main.java
@@ -0,0 +1,16 @@
+
+
+public class Tratamentoexc {
+
+    public static void main(String[] args) {
+        try{
+          
+          System.out.println(19384/0);
+            }catch(Exception excecao){
+                System.out.println("Ocorreu erro: "+ excecao.getMessage());
+            }System.out.println("fim");
+          }
+        }
+        
+    
+        //exception forçada / forced exception
