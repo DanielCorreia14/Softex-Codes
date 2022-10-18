c2c02f9c30b121d04f48a3dbf85ff2082c4e06e9
Daniel Mangabeira Correia <107889248+DanielCorreia14@users.noreply.github.com>
2022-10-18 18:19:09 -0300

    Create main.cs

---
+++ adapter/main.cs
@@ -0,0 +1,49 @@
+abstract class Pato
+{
+    public string fazerQuaQua()
+    {
+        return "quaqua";
+    }
+
+public string voar() 
+{
+    return "voavoa";
+}
+
+
+class GalinhaAdapter extends Pato
+{
+}
+
+abstract class Galinha
+{
+    public string fazerCocorico()
+    {
+        return "cocorico";
+    }
+
+public string voar()
+{
+    return "voar";
+}
+}
+
+class GalinhaCarijo extends Galinha
+{
+    public Galinha(string galinha)
+    {
+        private this.galinha=Galinha;
+    }
+}
+
+public class galinha
+{
+}
+
+class AdaptadorPato extends Pato
+{
+}
+
+class AdaptadorPatoDemo extends AdaptadorPato
+{
+}
